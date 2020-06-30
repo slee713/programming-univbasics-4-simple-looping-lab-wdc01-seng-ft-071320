c5c63f3b05b018ef68c252de885c9dbbf13d1d56
Steven Lee <steven.lee1393@gmail.com>
2020-06-30 15:27:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,14 @@
 # Write your methods here
 def loop_message_five_times(message)
   counter = 1
-  while counter <= 5
-    puts messages
+  while counter <= 5 do
+    puts message
   end
+end
+
+def loop_message_n_times(message, number)
+  counter =0
+  while counter < number do
+    puts message
+  end
+end
